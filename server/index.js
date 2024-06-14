4b8655e222e2f44e08504be46bce14fb53c0eb7c
nakshatraa <nakshatraachavan@gmail.com>
2024-06-14 21:48:28 +0530

    Add dotenv support and update .gitignore to exclude .env

--- server/index.js
+++ server/index.js
@@ -2,26 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-//import dotenv from 'dotenv'; 
+import dotenv from 'dotenv'; 
 import postRoutes from './routes/posts.js';
 // import userRoutes from './routes/users.js';
 
+dotenv.config();
+
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-// app.use('/user',userRoutes);
+// app.use('/user', userRoutes);
 
-const CONNECTION_URL='mongodb+srv://souvenirs:souvenirs123@cluster0.sgrjvdn.mongodb.net/'
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
- //mongoose.set('useFindAndModify', false);
-  
-  
-  
+// mongoose.set('useFindAndModify', false);
